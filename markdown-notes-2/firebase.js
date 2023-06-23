b309e46948e87795e30f6d8b4f1184b69ad4bc15
Michael Dorst <m@mdorst.net>
2023-06-23 13:02:23 -0700

    Add markdown-notes-2

---
+++ markdown-notes-2/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQyo4-db6Go9p7ROgyWruAcbe89PZS0I0",
+  authDomain: "react-notes-1a71d.firebaseapp.com",
+  projectId: "react-notes-1a71d",
+  storageBucket: "react-notes-1a71d.appspot.com",
+  messagingSenderId: "605123689662",
+  appId: "1:605123689662:web:aa10e8882d132adbf932ef"
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const notesCollection = collection(db, "notes")
